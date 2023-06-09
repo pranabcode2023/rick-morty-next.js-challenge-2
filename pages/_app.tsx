e46f0db4eba189ce4b812bc270172488da4db677
pranabcode2023 <pranabcode2023@gmail.com>
2023-06-09 17:20:34 +0200

    app wrap by apollo provider and working on episodes page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache,ApolloProvider } from '@apollo/client'
+
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/graphql",
+  cache: new InMemoryCache()
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <ApolloProvider client={client}>
      <Layout>
     <Component {...pageProps} />
     </Layout>
+    </ApolloProvider>
   )
  
 }
--- pages/episodes/episodes.tsx
+++ pages/episodes/episodes.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
- const episodes = () => {
-  return (
-    <div>episodes</div>
-  )
+
+import { gql, useQuery } from '@apollo/client';
+
+const GET_EPISODE = gql`
+query getEpisode{
+  characters{
+      results{
+          image
+          name
+          gender
+          location{
+              name
+          }
+          episode{
+            name
+            created
+            characters{
+                name
+                image
+            }
+            
+          }
+      }
+  }
 }
-export default episodes;
+
+`;
+
+function Episodes({}) {
+  // const { loading, error, data } = useQuery(GET_EPISODE);
+
+  // if (loading) return 'Loading...';
+  // if (error) return `Error! ${error.message}`;
+
+  // return (
+  //   <select name='character' onChange={ characters}>
+  //     {data.dogs.map((Characters) => (
+  //       <option key={characters.name} value={characters.results}>
+  //         {characters.results}
+  //       </option>
+  //     ))}
+  //   </select>
+  // );
+}
+
+export default  Episodes;
+
+
+
