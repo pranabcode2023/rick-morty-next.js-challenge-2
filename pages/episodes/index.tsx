e550fdd53bd225d88ee81022e3aceb928feea00b
pranabcode2023 <pranabcode2023@gmail.com>
2023-06-15 15:57:55 +0200

    some changes

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -1,53 +1,47 @@
-import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
 // import { GET_EPISODE } from '../../graphql/queries';
-
 
 const GET_EPISODE = gql`
   query getEpisodes {
     episodes {
-    results {
-      episode
-      created
-      characters {
-        image
-        name
+      results {
+        id
+        episode
+        created
+        characters {
+          id
+          image
+          name
+        }
       }
     }
-  }
   }
 `;
 
 const Episodes = () => {
   const { loading, error, data } = useQuery(GET_EPISODE);
-  
+  console.log("data", data);
 
-  if (loading) return 'Loading...';
+  if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
 
   return (
     <div>
-      
-        {data.episodes.results.map((result: any) => (
-          <div key={result.characters.name}>
-            <p>
-            {result.characters.name}
-            </p>
-           
-          </div>
-        ))}
-      
+      {data.episodes.results.map((result: any) => (
+        <div key={result.id}>
+          {result.characters.map((c) => {
+            return (
+              <div key={c.id}>
+                {" "}
+                <p>{c.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 };
 
-
 export default Episodes;
-
-  
-  
-  
-  
-  
-
-
