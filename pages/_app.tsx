b6b537ef194a424390cd5b27f34c46a0ebd2f596
pranabcode2023 <pranabcode2023@gmail.com>
2023-06-26 20:27:55 +0200

    remove episode from layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,23 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ApolloClient, InMemoryCache,ApolloProvider } from '@apollo/client'
-import Episodes from './episodes';
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
     uri: "https://rickandmortyapi.com/graphql",
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
   return (
     <ApolloProvider client={client}>
-     <Layout>
-     <Component {...pageProps} />
-     <h1>Episodes</h1>
-     <Episodes/>
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ApolloProvider>
-  )
- 
+  );
 }
 
-export default MyApp
-
+export default MyApp;
 
 // import { NextPage } from 'next';
 // import Layout from '../components/Layout'
@@ -35,14 +30,14 @@ export default MyApp
 // };
 
 // type AppPropsWithLayout = AppProps & {
-//   Component: NextPageWithLayout 
+//   Component: NextPageWithLayout
 // }
 
 // function MyApp({ Component, pageProps }:  AppPropsWithLayout) {
 
 //   const getLayout = Component.getLayout ?? ( (page) =>page);
 //   return getLayout (<Component {...pageProps} /> );
- 
+
 // }
 
 // export default MyApp
